2ae3f075b6e423ac013ffae1787b09b58d2ab075
Thomas Schersach <toj315@gmail.com>
2017-11-20 11:39:20 +0100

    Added Dummy AskerBehavior

--- src/main/java/fr/m2/miage/pharma/behaviors/ResponderBehaviour.java
+++ src/main/java/fr/m2/miage/pharma/behaviors/ResponderBehaviour.java
@@ -3,14 +3,8 @@ package fr.m2.miage.pharma.behaviors;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
 
 public class ResponderBehaviour extends CyclicBehaviour {
-
-  private final static MessageTemplate templateRequest = MessageTemplate
-      .MatchPerformative(ACLMessage.REQUEST);
-  private final static MessageTemplate templateAgree = MessageTemplate
-      .MatchPerformative(ACLMessage.AGREE);
 
   public ResponderBehaviour(Agent agent) {
     super(agent);
