aebf38599e4866c1b434c7daea2e540630024738
Thomas Schersach <toj315@gmail.com>
2017-11-28 15:20:56 +0100

    Refactored main + added SessionFactory Provider

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,26 @@
+import static fr.m2.miage.pharma.services.HibernateSessionProvider.getSessionFactory;
+
 import fr.m2.miage.pharma.models.Client;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
 
-//  private static final SessionFactory ourSessionFactory;
-//
-//  static {
-//    try {
-//      ourSessionFactory = new Configuration().
-//          configure("hibernate.cfg.xml").
-//          buildSessionFactory();
-//    } catch (Throwable ex) {
-//      throw new ExceptionInInitializerError(ex);
-//    }
-//  }
-//
-//  public static Session getSession() throws HibernateException {
-//    return ourSessionFactory.openSession();
-//  }
-//
-//  public static void main(final String[] args) throws Exception {
-//    final Session session = getSession();
-//    try {
-//      System.out.println("querying all the managed entities...");
-//      final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
-//      for (Object key : metadataMap.keySet()) {
-//        final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
-//        final String entityName = classMetadata.getEntityName();
-//        final Query query = session.createQuery("from " + entityName);
-//        System.out.println("executing: " + query.getQueryString());
-//        for (Object o : query.list()) {
-//          System.out.println("  " + o);
-//        }
-//      }
-//    } finally {
-//      session.close();
-//    }
-//  }
-
   public static void main(String[] args) {
-    final StandardServiceRegistry registry =
-        new StandardServiceRegistryBuilder()
-            .configure("hibernate.cfg.xml")
-            .build();
-    SessionFactory sessionFactory = new MetadataSources(registry)
-        .buildMetadata()
-        .buildSessionFactory();
 
-    Session session = sessionFactory.openSession();
-    Transaction tx = session.beginTransaction();
+    // On peut récupérer globalement des sessions
+    Session session = getSessionFactory().openSession();
+    session.beginTransaction(); // Ouvre la tx
 
     Client client = new Client();
     client.setName("Arnaud");
 
-    session.persist(client);
-    tx.commit();
-    session.close();
-    sessionFactory.close();
+    session.save(client); // On utilise save et non persist
+    session.getTransaction().commit(); // On commit
+    session.close(); // On oublie pas de fermer la session
+
+    // On peut faire des named queries et tout et tout comme dab.
+
+    // A utiliser que si l'application a fini de tourner
+    getSessionFactory().close();
   }
 }
--- src/main/java/fr/m2/miage/pharma/services/HibernateSessionProvider.java
+++ src/main/java/fr/m2/miage/pharma/services/HibernateSessionProvider.java
@@ -1,13 +1,28 @@
 package fr.m2.miage.pharma.services;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateSessionProvider {
 
-//  private static SessionFactory sessionFactory;
-//
-//  public static SessionFactory getSessionFactory() {
-//    if (sessionFactory == null) {
-//      AnnotationCon
-//    }
-//  }
+  private static SessionFactory sessionFactory;
 
+  public static SessionFactory getSessionFactory() {
+    if (sessionFactory == null) {
+      final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+          .configure()
+          .build();
+      try {
+        sessionFactory = new MetadataSources(registry)
+            .buildMetadata()
+            .buildSessionFactory();
+      } catch (Exception e) {
+        StandardServiceRegistryBuilder.destroy(registry);
+      }
+    }
+
+    return sessionFactory;
+  }
 }
